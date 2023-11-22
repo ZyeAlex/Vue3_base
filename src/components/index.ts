82280fd64418b09e4a6c69c7ff2a8251d044bcb7
ZyeAlex <76239010@qq.com>
2023-11-22 16:29:02 +0800

    第二次提交

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,6 +2,6 @@ import type { App } from 'vue'
 import EchartsAxis from './echarts/axis/index.vue'
 import EchartsResource from './echarts/resource.vue'
 export const setupGlobComponent = (app: App<Element>): void => {
-  app.component('EchartsAxis', EchartsAxis)
-  app.component('EchartsResource', EchartsResource)
+  // app.component('EchartsAxis', EchartsAxis)
+  // app.component('EchartsResource', EchartsResource)
 }
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { createPinia } from 'pinia'
 import { setupGlobComponent } from '@/components'
 import App from './App.vue'
 import router from './router'
-
 const app = createApp(App)
 
 // pinia 
