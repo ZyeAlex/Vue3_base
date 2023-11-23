eb21f3ac42dedef34c01f39d2e4cae3576debd35
ZyeAlex <76239010@qq.com>
2023-11-23 16:32:21 +0800

    123

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,8 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
-import Theme from '@/components/theme/theme.vue'
 </script>
 
 <template>
-  <header>
-    <Theme></Theme>
-  </header>
   <RouterView />
 </template>
 
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import test from '@/components/element/form/test.vue'
+import Theme from '@/components/theme/theme.vue'
 </script>
 
 <template>
+    <theme></theme>
     <test />
 </template>
 
