022931fd6175ec279c79f8e1a077bee732ed2a66
ZyeAlex <76239010@qq.com>
2023-11-23 16:34:55 +0800

    提交

--- src/components/element/form/test.vue
+++ src/components/element/form/test.vue
@@ -7,16 +7,23 @@ const form = reactive({
     sex: 0,
     message: '你好，世界！'
 })
-setTimeout(() => {
-    console.log(form);
-}, 1000);
+const sex = [
+    {
+        label: '男',
+        value: 1
+    },
+    {
+        label: '女',
+        value: 0
+    }
+]
 </script>
 
 <template>
     <v-form v-model="form">
         <v-form-item label="姓名" prop="name" />
         <v-form-item label="年龄" prop="age" type="number" />
-        <v-form-item label="性别" prop="sex" type="select" option=""  ></v-form-item>
+        <v-form-item label="性别" prop="sex" type="select" option=""></v-form-item>
         <v-form-item label="信息" prop="message" />
     </v-form>
 </template>
