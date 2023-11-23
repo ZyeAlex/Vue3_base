2d5b9003e5bfb8ef561943d838a0465dd9474a8e
ZyeAlex <76239010@qq.com>
2023-11-23 17:26:24 +0800

    合并

--- src/components/element/form/test.vue
+++ src/components/element/form/test.vue
@@ -2,9 +2,10 @@
 import { reactive } from 'vue'
 
 const form = reactive({
-    name: 'cy',
+    name: 'fsk',
     age: 18,
     sex: 0,
+    font:['sing','jump','rap','basketball'],
     message: '你好，世界！'
 })
 const sex = [
@@ -17,6 +18,24 @@ const sex = [
         value: 0
     }
 ]
+const font = [
+    {
+        label: 'sing',
+        value: 'sing'
+    },
+    {
+        label: 'jump',
+        value: 'jump'
+    },
+    {
+        label: 'rap',
+        value: 'rap'
+    },
+    {
+        label: 'basketball',
+        value: 'basketball'
+    },
+]
 
 </script>
 
@@ -25,7 +44,8 @@ const sex = [
         <v-form v-model="form">
             <v-form-item label="姓名" prop="name" maxlength="5" />
             <v-form-item label="年龄" prop="age" type="number" :max="90" />
-            <v-form-item label="性别" prop="sex" type="select"></v-form-item>
+            <v-form-item label="性别" prop="sex" type="select" :option="sex"></v-form-item>
+            <v-form-item label="爱好" prop="font" type="select" :option="font" multiple></v-form-item>
             <v-form-item label="信息" prop="message" type="textarea" />
         </v-form>
         <el-button @click="() => console.log(form)">提交</el-button>
