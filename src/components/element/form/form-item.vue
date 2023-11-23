697474bbc88620edf6bc42ca85a57778f34824b2
Yu-Chen97 <1412305428@qq.com>
2023-11-23 17:10:38 +0800

    form

--- src/components/element/form/form-item.vue
+++ src/components/element/form/form-item.vue
@@ -1,25 +1,47 @@
 <script setup lang="ts">
 withDefaults(defineProps<{
     prop: string
+    label?: string
+    labelWidth?: string | number
     type?: string
     placeholder?: string
+    option?: any[]
+    required?: boolean
+    error?: string
+    showMessage?: boolean
+    inlineMessage?: string | boolean
+    size?: 'large' | 'default' | 'small'
+    validateStatus?: 'error' | 'validating' | 'success'
 }>(), {
     type: 'text',
-    placeholder: '请输入'
+    placeholder: '请输入',
+    required: false,
+    rules: {},
+    option: () => [],
 })
 import { inject } from 'vue'
 const model = inject('model')
 </script>
 
 <template>
-    <el-form-item v-if="type == 'text'" :prop="prop">
-        <el-input v-model="model[prop]" type="text" :placeholder="placeholder" />
+    <el-form-item v-if="type == 'text'" v-bind="$props">
+        <el-input v-model="model[prop]" type="text" :placeholder="placeholder" v-bind="$attrs" />
     </el-form-item>
-    <el-form-item v-if="type == 'number'" :prop="prop">
-        <el-input v-model="model[prop]" type="number" :placeholder="placeholder" />
+    <el-form-item v-if="type == 'number'" v-bind="$props">
+        <el-input v-model.number="model[prop]" type="number" :placeholder="placeholder" v-bind="$attrs" />
     </el-form-item>
-    <el-form-item v-if="type == 'textarea'" :prop="prop">
-        <el-input v-model="model[prop]" type="textarea" :placeholder="placeholder" />
+    <el-form-item v-if="type == 'textarea'" v-bind="$props">
+        <el-input v-model="model[prop]" type="textarea" :placeholder="placeholder" v-bind="$attrs" />
+    </el-form-item>
+    <el-form-item v-if="type == 'select'" v-bind="$props">
+        <el-select v-model="model[prop]" style="width: 100%;" v-bind="$attrs">
+            <el-option v-for="item in option" :key="item.value" :label="item.label" :value="item.value"></el-option>
+        </el-select>
+    </el-form-item>
+    <el-form-item v-if="type == 'selectTree'" v-bind="$props">
+        <el-select v-model="model[prop]" style="width: 100%;" v-bind="$attrs">
+            <el-option v-for="item in option" :key="item.value" :label="item.label" :value="item.value"></el-option>
+        </el-select>
     </el-form-item>
 </template>
 
