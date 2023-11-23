f9500aeb299e85a094f8c7041a3fa676e6b7f170
fengsaikang <2693061212@qq.com>
2023-11-23 20:28:53 +0800

    style: 代码结构

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,6 @@ import { RouterView } from 'vue-router';
 </script>
 
 <template>
-  <header>
-    <Theme></Theme>
-    <VIcon icon="svg-icon:icon" size="30" :is-theme="true"></VIcon>
-    <VIcon icon="ep:search" :size="30" color="var(--el-color-primary)" :is-theme="true" hover-color="var(--el-color-info)" />
-    <div class="text p-1 m-1 ml-3 px-4"></div>
-  </header>
   <RouterView />
 </template>
 
--- src/views/home.vue
+++ src/views/home.vue
@@ -5,6 +5,9 @@ import Theme from '@/components/theme/theme.vue'
 
 <template>
     <theme></theme>
+    <VIcon icon="svg-icon:icon" size="30" :is-theme="true"></VIcon>
+    <VIcon icon="ep:search" :size="30" color="var(--el-color-primary)" :is-theme="true" hover-color="var(--el-color-info)" />
+    <div class="text p-1 m-1 ml-3 px-4"></div>
     <test />
 </template>
 
