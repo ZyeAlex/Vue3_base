734c50adcc3d2ea57b0a50bb55beb0fa3fc1d1d6
ZyeAlex <76239010@qq.com>
2023-11-23 17:11:58 +0800

    hebin

--- src/components/element/form/test.vue
+++ src/components/element/form/test.vue
@@ -2,8 +2,8 @@
 import { reactive } from 'vue'
 
 const form = reactive({
-    name: 'cy',
-    age: 80,
+    name: '陈宇',
+    age: 18,
     sex: 0,
     message: '你好，世界！'
 })
@@ -24,7 +24,7 @@ const sex = [
         <v-form-item label="姓名" prop="name" />
         <v-form-item label="年龄" prop="age" type="number" />
         <v-form-item label="性别" prop="sex" type="select" :option="sex"></v-form-item>
-        <v-form-item label="信息" prop="message" type="textarea"/>
+        <v-form-item label="信息" prop="message" type="textarea" />
     </v-form>
 </template>
 
